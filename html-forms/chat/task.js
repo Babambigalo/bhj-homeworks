2c4e8c7215428b9372f437a26f97745e17f8de3d
Matveev <myatiy963@gmail.com>
2019-03-25 14:45:57 +0300

    html form has done

--- html-forms/chat/task.js
+++ html-forms/chat/task.js
@@ -2,15 +2,34 @@ function chatHandler() {
     const chatWidget = document.querySelector('.chat-widget');
     const input = document.getElementById('chat-widget__input');
     const messages = document.querySelector( '.chat-widget__messages' );
-    const chat_area = document.querySelector('.chat-widget__area');
+    const widgetMessages = document.querySelector('.chat-widget__messages-container');
+    let timer=0;
     const respons = ['Чё надо?!','цыгль цыгль ай лю лю','фак ю бич пососи кирпич', 'чё пацаны анимэ?!','Гамарджобат','салам','Добрый день', 'пнх', 'пасть порву, моргалы выколю', 'пшёл вон отсюда щегол','eeee боооооой'];
-    console.log(chatWidget);
+    
     chatWidget.addEventListener('click',()=>{
         chatWidget.classList.add('chat-widget_active');
+        setInterval(()=>{
+            if(timer === 30) {
+                let hours = new Date().getHours();
+                let minutes = new Date().getMinutes();
+                if (hours < 10) hours = '0' + hours;
+                if (minutes<10) minutes = '0' + minutes;
+                let time = hours + ':' + minutes;
+                messages.innerHTML += `
+                <div class="message">
+                    <div class="message__time">${time}</div>
+                    <div class="message__text">${respons[Math.round(Math.random()*10)]}</div>
+                </div>`;
+                timer = 0;
+            }else{
+                timer++;
+            }         
+        },1000);
     });
 
     input.addEventListener('keydown',(event)=>{
         if(event.key === 'Enter' && input.value.length != 0){
+            timer = 0;
             let text = input.value;
             let hours = new Date().getHours();
             let minutes = new Date().getMinutes();
@@ -30,9 +49,9 @@ function chatHandler() {
                 <div class="message__text">${respons[Math.round(Math.random()*10)]}</div>
             </div>`;
 
-            let lastChild = messages.lastElementChild;
-            chat_area.scrollTop = chat_area.scrollHeight;
-            console.log(Math.round(Math.random()*10));
+            widgetMessages.scrollTop = widgetMessages.scrollHeight;
+            widgetMessages.style.overflow = 'hidden';
+        
         }
         
     });
