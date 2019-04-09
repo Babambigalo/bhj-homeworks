fd61a404a35a8a81d29f094aed5ba91171241f0f
Matveev <myatiy963@gmail.com>
2019-04-09 14:01:27 +0300

    client state popup has refactored

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,5 +1,18 @@
 const modal = document.getElementById('subscribe-modal');
-if(document.cookie === 'closed=true') modal.classList.remove('modal_active');
+
+const getCookie = (name) => {
+    const value = '; ' + document.cookie;
+    let parts = value.split('; ' + name + '=');
+    if(parts.length === 2) {
+        return parts
+        .pop()
+        .split(';')
+        .shift();
+    }
+}
+
+if(getCookie('closed')) modal.classList.remove('modal_active');
+
 if(modal !== null) {
     const close = document.querySelector('.modal__close');
     console.log(modal,close);
